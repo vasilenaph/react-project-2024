51b55a711d478d9b550c09f141c99d2ca22cff51
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-08-02 20:36:15 +0300

    Add abstract requester

--- client/src/components/carlist/CarList.jsx
+++ client/src/components/carlist/CarList.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 // Sample data
 const products = [
@@ -24,6 +23,7 @@ const products = [
 ];
 
 export default function CarList() {
+
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
